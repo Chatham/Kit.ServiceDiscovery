17b68845040a9b7059dcbfcc7f6e0c2d67968bef
Chris Hoffman <choffman@chathamfinancial.com>
2016-08-25 22:48:29 -0400

    Adding load balancer algorithms for service subscribers

---
+++ src/Chatham.ServiceDiscovery.Abstractions/IServiceLoadBalancer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chatham.ServiceDiscovery.Abstractions
+{
+    public interface IServiceLoadBalancer
+    {
+        Task<Uri> Endpoint();
+    }
+}
---
+++ src/Chatham.ServiceDiscovery.LoadBalancer/RandomLoadBalancer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Chatham.ServiceDiscovery.Abstractions;
+
+namespace Chatham.ServiceDiscovery.LoadBalancer
+{
+    public class RandomLoadBalancer : IServiceLoadBalancer
+    {
+        private readonly Random _random;
+        private readonly IServiceSubscriber _subscriber;
+
+        public RandomLoadBalancer(IServiceSubscriber subscriber)
+        {
+            _subscriber = subscriber;
+            _random = new Random();
+        }
+
+        public RandomLoadBalancer(IServiceSubscriber subscriber, int seed)
+        {
+            _subscriber = subscriber;
+            _random = new Random(seed);
+        }
+
+         public async Task<Uri> Endpoint()
+        {
+            var endpoints = await _subscriber.Endpoints();
+            if (endpoints == null || endpoints.Count == 0)
+            {
+                return null;
+            }
+
+            return endpoints[_random.Next(endpoints.Count) - 1];
+        }
+    }
+}
