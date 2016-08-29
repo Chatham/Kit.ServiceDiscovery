f2275f40243e86d5722367a100dcc19dbb7fc4ec
Chris Hoffman <choffman@chathamfinancial.com>
2016-08-29 13:54:22 -0400

    Creating ServiceEndpoint class to properly encapsulate what is returned from service discovery replacing Uri

---
+++ src/Chatham.Kit.ServiceDiscovery.Abstractions/ServiceEndpoint.cs
@@ -0,0 +1,13 @@
+﻿namespace Chatham.Kit.ServiceDiscovery.Abstractions
+{
+    public class ServiceEndpoint
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Host}:{Port}";
+        }
+    }
+}
