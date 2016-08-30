b0fd75877f201ab7a94b638a6b579e84be7590c0
Chris Hoffman <choffman@chathamfinancial.com>
2016-08-30 11:34:53 -0400

    Adding random load balancer tests

--- test/Chatham.Kit.ServiceDiscovery.LoadBalancer.Tests/RandomLoadBalancerTests.cs
+++ test/Chatham.Kit.ServiceDiscovery.LoadBalancer.Tests/RandomLoadBalancerTests.cs
@@ -46,5 +46,34 @@ namespace Chatham.Kit.ServiceDiscovery.LoadBalancer.Tests
                 Assert.AreEqual(endpoint.Port, endpointActual.Port);
             }
         }
+
+        [TestMethod]
+        public async Task Endpoint_MultipleEndpoints_ReturnsEndpointInRandomOrder()
+        {
+            var expectedList = new List<Endpoint>
+            {
+                new Endpoint {Host = Guid.NewGuid().ToString(), Port = 1},
+                new Endpoint {Host = Guid.NewGuid().ToString(), Port = 2},
+                new Endpoint {Host = Guid.NewGuid().ToString(), Port = 3}
+            };
+
+            var subscriber = Substitute.For<IServiceSubscriber>();
+            subscriber.Endpoints().Returns(Task.FromResult(expectedList));
+            var lb = new RandomLoadBalancer(subscriber, 0);
+
+
+            var actual = new List<Endpoint>();
+            for (var i = 0; i < 10; i++)
+            {
+                actual.Add(await lb.Endpoint());
+            }
+
+            var expectedOrder = new List<int> {2, 2, 2, 1, 0, 1, 2, 1, 2, 0};
+            for (var i = 0; i < actual.Count; i++)
+            {
+                Assert.AreEqual(expectedList[expectedOrder[i]].Host, actual[i].Host);
+                Assert.AreEqual(expectedList[expectedOrder[i]].Port, actual[i].Port);
+            }
+        }
     }
 }
