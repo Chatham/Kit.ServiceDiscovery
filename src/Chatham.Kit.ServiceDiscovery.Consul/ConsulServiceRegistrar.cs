58bce9849e01989211c49a61db8d2361920ac8eb
Chris Hoffman <choffman@chathamfinancial.com>
2016-08-30 21:24:43 -0400

    Adding ServiceRegistar implementation for Consul

---
+++ src/Chatham.Kit.ServiceDiscovery.Consul/ConsulServiceRegistrar.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Chatham.Kit.ServiceDiscovery.Abstractions;
+using Consul;
+using Microsoft.Extensions.Logging;
+
+namespace Chatham.Kit.ServiceDiscovery.Consul
+{
+    public class ConsulServiceRegistrar : IServiceRegistrar
+    {
+        private readonly ILogger _log;
+        private readonly IConsulClient _client;
+        private readonly AgentServiceRegistration _serviceRegistration;
+
+        public ConsulServiceRegistrar(ILoggerFactory log, IConsulClient client, AgentServiceRegistration serviceRegistration)
+        {
+            _log = log.CreateLogger(nameof(ConsulServiceRegistrar));
+            _client = client;
+            _serviceRegistration = serviceRegistration;
+        }
+
+        public async Task Register()
+        {
+            var registrationResult = await _client.Agent.ServiceRegister(_serviceRegistration);
+            if (IsSuccessfulStatus(registrationResult.StatusCode))
+            {
+                _log.LogInformation(
+                    $"Succesfully registered service {_serviceRegistration.Name} at {_serviceRegistration.Address}:{_serviceRegistration.Port}");
+            }
+            else
+            {
+                throw new Exception($"Unable to register {_serviceRegistration.Name} at {_serviceRegistration.Address}:{_serviceRegistration.Port}");
+            }
+        }
+
+        public async Task Deregister()
+        {
+            var deregistrationResult = await _client.Agent.ServiceDeregister(_serviceRegistration.ID);
+            if (IsSuccessfulStatus(deregistrationResult.StatusCode))
+            {
+                _log.LogInformation(
+                    $"Deregistered service {_serviceRegistration.Name} [{_serviceRegistration.ID}]");
+            }
+            else
+            {
+                throw new Exception($"Unable to deregister {_serviceRegistration.Name}  [{_serviceRegistration.ID}]");
+            }
+        }
+
+        private static bool IsSuccessfulStatus(HttpStatusCode code)
+        {
+            return (int)code >= 200 && (int)code <= 299;
+        }
+    }
+}
---
+++ src/Chatham.Kit.ServiceDiscovery.Consul/ConsulServiceRegistrarFactory.cs
@@ -0,0 +1,23 @@
+﻿using Chatham.Kit.ServiceDiscovery.Abstractions;
+using Consul;
+using Microsoft.Extensions.Logging;
+
+namespace Chatham.Kit.ServiceDiscovery.Consul
+{
+    public class ConsulServiceRegistrarFactory
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly IConsulClient _client;
+
+        public ConsulServiceRegistrarFactory(ILoggerFactory loggerFactory, IConsulClient client)
+        {
+            _loggerFactory = loggerFactory;
+            _client = client;
+        }
+
+        public IServiceRegistrar CreateConsulRegistrar(AgentServiceRegistration service)
+        {
+            return new ConsulServiceRegistrar(_loggerFactory, _client, service);
+        }
+    }
+}
