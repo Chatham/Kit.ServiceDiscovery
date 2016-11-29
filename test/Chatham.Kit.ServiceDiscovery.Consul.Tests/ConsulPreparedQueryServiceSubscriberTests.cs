3cdd4ad108866fb6f332dab041faac5c49c6dd5c
Chris Hoffman <choffman@chathamfinancial.com>
2016-11-29 18:37:47 -0500

    Adding tests

--- test/Chatham.Kit.ServiceDiscovery.Consul.Tests/ConsulPreparedQueryServiceSubscriberTests.cs
+++ test/Chatham.Kit.ServiceDiscovery.Consul.Tests/ConsulPreparedQueryServiceSubscriberTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Consul;
 using Xunit;
@@ -25,5 +26,45 @@ namespace Chatham.Kit.ServiceDiscovery.Consul.Tests
             Assert.NotNull(actual);
             Assert.Equal(0, actual.Count);
         }
+
+        [Fact]
+        public async Task Endpoints_withLotsOfData_returnsList()
+        {
+            var fixture = new ConsulPreparedQueryServiceSubscriberFixture();
+            fixture.ServiceName = Guid.NewGuid().ToString();
+
+            var services = new List<ServiceEntry>();
+            for (var i = 0; i < 5; i++)
+            {
+                services.Add(new ServiceEntry
+                {
+                    Node = new Node
+                    {
+                        Address = Guid.NewGuid().ToString()
+                    },
+                    Service = new AgentService
+                    {
+                        Address = Guid.NewGuid().ToString(),
+                        Port = 123
+                    }
+                });
+            }
+
+            fixture.ClientQueryResult = new QueryResult<PreparedQueryExecuteResponse>
+            {
+                Response = new PreparedQueryExecuteResponse{
+                    Nodes = services.ToArray()
+                }
+            };
+
+            fixture.SetPreparedQueryEndpoint();
+
+            var subscriber = fixture.CreateSut();
+            var actual = await subscriber.Endpoints();
+
+            Assert.NotNull(actual);
+            Assert.Equal(services.Count, actual.Count);
+        }
+
     }
 }
