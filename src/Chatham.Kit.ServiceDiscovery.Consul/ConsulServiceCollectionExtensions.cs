a98a979caaffbc8c6269a0bdf69e28c37644b323
Chris Hoffman <choffman@chathamfinancial.com>
2016-11-29 20:00:41 -0500

    CacheServiceSubscriberExtensions -> CacheServiceCollectionExtensions

--- src/Chatham.Kit.ServiceDiscovery.Consul/ConsulServiceCollectionExtensions.cs
+++ src/Chatham.Kit.ServiceDiscovery.Consul/ConsulServiceCollectionExtensions.cs
@@ -24,7 +24,7 @@ namespace Chatham.Kit.ServiceDiscovery.Consul
             services.AddCacheServiceSubscriber();
 
             services.TryAdd(new ServiceDescriptor(typeof(IConsulClient), p => new ConsulClient(config), ServiceLifetime.Singleton));
-            services.TryAddSingleton<IPollingServiceSubscriberFactory, CacheConsulServiceSubscriberFactory>();
+            services.TryAddSingleton<IPollingServiceSubscriberFactory, ConsulCacheServiceSubscriberFactory>();
             services.TryAddSingleton<IConsulServiceRegistrarFactory, ConsulServiceRegistrarFactory>();
 
             return services;
