8f07a85078c68a524c7937f2ce2ee27834901031
Chris Hoffman <choffman@chathamfinancial.com>
2017-01-26 15:14:05 -0500

    Adding FixedServiceSubscriber

---
+++ src/Chatham.Kit.ServiceDiscovery.Fixed/FixedSubscriber.cs
@@ -0,0 +1,26 @@
+﻿using Chatham.Kit.ServiceDiscovery.Abstractions;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chatham.Kit.ServiceDiscovery.Fixed
+{
+    public class FixedSubscriber : IServiceSubscriber
+    {
+        private readonly List<Endpoint> _endpoints;
+
+        public FixedSubscriber(List<Endpoint> endpoints)
+        {
+            _endpoints = endpoints;
+        }
+        
+        public Task<List<Endpoint>> Endpoints(CancellationToken ct = default(CancellationToken))
+        {
+            return Task.FromResult(_endpoints);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
