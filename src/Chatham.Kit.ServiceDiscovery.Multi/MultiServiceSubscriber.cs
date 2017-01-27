731ae0ee62961c6934c306565bf530bc95581c33
Chris Hoffman <choffman@chathamfinancial.com>
2017-01-27 15:47:23 -0500

    Adding Multi subscriber

---
+++ src/Chatham.Kit.ServiceDiscovery.Multi/MultiServiceSubscriber.cs
@@ -0,0 +1,38 @@
+﻿using Chatham.Kit.ServiceDiscovery.Abstractions;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chatham.Kit.ServiceDiscovery.Multi
+{
+    public class MultiServiceSubscriber : IServiceSubscriber
+    {
+        private readonly List<IServiceSubscriber> _serviceSubscribers;
+
+        public MultiServiceSubscriber(List<IServiceSubscriber> serviceSubscribers)
+        {
+            _serviceSubscribers = serviceSubscribers;
+        }
+
+        public async Task<List<Endpoint>> Endpoints(CancellationToken ct = new CancellationToken())
+        {
+            foreach (var subscriber in _serviceSubscribers)
+            {
+                var endpoints = await subscriber.Endpoints(ct).ConfigureAwait(false);
+                if (endpoints.Count > 0)
+                {
+                    return endpoints;
+                }
+            }
+            return new List<Endpoint>();
+        }
+
+        public void Dispose()
+        {
+            foreach (var subscriber in _serviceSubscribers)
+            {
+                subscriber.Dispose();
+            }
+        }
+    }
+}
---
+++ test/Chatham.Kit.ServiceDiscovery.Multi.Tests/MultiServiceSubscriberTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Chatham.Kit.ServiceDiscovery.Abstractions;
+using Chatham.Kit.ServiceDiscovery.Fixed;
+using Xunit;
+
+namespace Chatham.Kit.ServiceDiscovery.Multi.Tests
+{
+    public class MultiServiceSubscriberTests
+    {
+        [Fact]
+        public async Task SingleSubscriber_ReturnsEndpoints()
+        {
+            var subscriber = new FixedSubscriber(new List<Endpoint> {new Endpoint()});
+            var multiSubscriber = new MultiServiceSubscriber(new List<IServiceSubscriber> { subscriber });
+            var endpoints = await multiSubscriber.Endpoints();
+            Assert.NotEmpty(endpoints);
+            Assert.Equal(1, endpoints.Count);
+            Assert.Equal(await subscriber.Endpoints(), endpoints);
+        } 
+    }
+}
