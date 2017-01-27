b6c38023ee52c3bc3d77b662a216887e19e9ad3a
Chris Hoffman <choffman@chathamfinancial.com>
2017-01-27 16:10:16 -0500

    Adding more multi subscriber tests

--- test/Chatham.Kit.ServiceDiscovery.Multi.Tests/MultiServiceSubscriberTests.cs
+++ test/Chatham.Kit.ServiceDiscovery.Multi.Tests/MultiServiceSubscriberTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Chatham.Kit.ServiceDiscovery.Abstractions;
 using Chatham.Kit.ServiceDiscovery.Fixed;
@@ -17,6 +18,40 @@ namespace Chatham.Kit.ServiceDiscovery.Multi.Tests
             Assert.NotEmpty(endpoints);
             Assert.Equal(1, endpoints.Count);
             Assert.Equal(await subscriber.Endpoints(), endpoints);
-        } 
+        }
+
+        [Fact]
+        public async Task AddMultipleSubscribers_FirstSubscriberReturnsData()
+        {
+            var subscriber1 = new FixedSubscriber(new List<Endpoint> { new Endpoint { Host = Guid.NewGuid().ToString() } });
+            var subscriber2 = new FixedSubscriber(new List<Endpoint> { new Endpoint { Host = Guid.NewGuid().ToString() } });
+            var multiSubscriber = new MultiServiceSubscriber(new List<IServiceSubscriber> { subscriber1, subscriber2 });
+            var endpoints = await multiSubscriber.Endpoints();
+            Assert.NotEmpty(endpoints);
+            Assert.Equal(1, endpoints.Count);
+            Assert.Equal(await subscriber1.Endpoints(), endpoints);
+        }
+
+        [Fact]
+        public async Task AddMultipleSubscribers_SecondSubscriberReturnsData()
+        {
+            var subscriber1 = new FixedSubscriber(new List<Endpoint>());
+            var subscriber2 = new FixedSubscriber(new List<Endpoint> { new Endpoint { Host = Guid.NewGuid().ToString() } });
+            var multiSubscriber = new MultiServiceSubscriber(new List<IServiceSubscriber> { subscriber1, subscriber2 });
+            var endpoints = await multiSubscriber.Endpoints();
+            Assert.NotEmpty(endpoints);
+            Assert.Equal(1, endpoints.Count);
+            Assert.Equal(await subscriber2.Endpoints(), endpoints);
+        }
+
+        [Fact]
+        public async Task AddMultipleSubscribers_NoneReturnReturnsEmptyList()
+        {
+            var subscriber1 = new FixedSubscriber(new List<Endpoint>());
+            var subscriber2 = new FixedSubscriber(new List<Endpoint>());
+            var multiSubscriber = new MultiServiceSubscriber(new List<IServiceSubscriber> { subscriber1, subscriber2 });
+            var endpoints = await multiSubscriber.Endpoints();
+            Assert.Empty(endpoints);
+        }
     }
 }
